802d26124381929eba5559b94f89e7c35751485c
@ewilliaml <willian_enrriquez89@hotmail.com>
2023-01-23 17:58:51 -0500

    starting

---
+++ src/test/java/com/willo/springbootreact/app/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willo.springbootreact.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
